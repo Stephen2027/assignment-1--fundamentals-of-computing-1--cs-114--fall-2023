2e0c63109e0af2b8423bafe12f47f46cfdce36c2
Stephen2027 <143655666+Stephen2027@users.noreply.github.com>
2023-09-20 17:07:18 -0400

    Update Assignment1.java
    fix on different parts of code

--- Assignment1.java
+++ Assignment1.java
@@ -7,6 +7,9 @@ import java.util.Random;
  */
 public class Assignment1 {
   public static void main(String[] args) {
+    int fahrenheit;    
+    int celsius;
+
     // print out initials
     System.out.println("SSSSSSSSSS        HH       HH");
     System.out.println("SSS     SS        HH       HH");
@@ -16,21 +19,21 @@ public class Assignment1 {
     System.out.println("SS     SSS        HH       HH");
     System.out.println("SSSSSSSSSS        HH       HH");
 
-
-    // Fahrenheit to Celcius
+        // Fahrenheit to Celcius
     Scanner input = new Scanner(System.in); {
-    System.out.println("\nPlease enter a number in degrees Fahernheit: ");
-    int Fahernheit = input.nextInt();
-    int Celsius = (Fahernheit - 32) * 5/9; // convert fahernheit to celcius
+    System.out.println("Please enter a number in degrees Fahernheit: ");
+    fahrenheit = input.nextInt();
+    celsius = (fahrenheit - 32); // celcius to fahrenheit
 
-
-    // takes five character string, reverses it and removes the first and last characters
+    // take five character string, reverses it and removes the first and last characters
     System.out.println("Please enter a five character string: ");
     String inputString = input.next();
     String reverseString = new StringBuilder(inputString.substring(1, 4)).reverse().toString();
 
     //asks for a random number between 32 and 16384 inclusive
-    Random rand = new Random();
+    System.out.println("Random number generated. Continuing...: ");
+    Random generator = new Random();
+    input.close();
     }
   }
 }
